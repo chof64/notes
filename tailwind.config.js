2cbad44a501a529ddaba749ccd25f8f265d8c40d
Chad Fernandez <developer@chadfernandez.me>
2024-02-28 11:27:36 +0800

    feat: updated header and footer and added theme.config on tailwindcss styling content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{mdx}", "./components/**/*.{mdx,tsx}"],
+  content: [
+    "./pages/**/*.{mdx}",
+    "./components/**/*.{mdx,tsx}",
+    "./theme.config.tsx",
+  ],
   theme: {
     extend: {},
   },
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Link from "next/link";
 
 const config: DocsThemeConfig = {
-  logo: <span className="!font-bold">chof64/notes</span>,
+  useNextSeoProps() {
+    return {
+      description: "A simple note-taking app.",
+      titleTemplate: "%s | Notes",
+    };
+  },
+  banner: {
+    text: "A work in progress. 🚧",
+  },
+  logo: <span className="font-bold">chof64/notes</span>,
   project: {
     link: "https://github.com/chof64/notes",
   },
   docsRepositoryBase: "https://github.com/chof64/notes",
   footer: {
-    text: "By Chad Fernandez. Built with Nextra.",
+    text: (
+      <div>
+        <div className="text-sm font-medium">
+          Copyright © 2024{" "}
+          <Link
+            className="underline underline-offset-2"
+            href="https://chadfernandez.me"
+            target="_blank"
+          >
+            Chad Fernandez
+          </Link>
+          . All rights reserved.
+        </div>
+        <div className="text-xs font-medium">
+          Made with ❤️, 🐼butter, and ☕️coffee.
+        </div>
+      </div>
+    ),
   },
 };
 
