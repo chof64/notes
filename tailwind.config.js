d68d48cfe87c8bb70ac0615510e7a942d69e1171
Chad Fernandez <developer@chadfernandez.me>
2024-02-28 09:24:34 +0800

    feat: added tailwind css and its configs and deps

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{mdx}", "./components/**/*.{mdx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
